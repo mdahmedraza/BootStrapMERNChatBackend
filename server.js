f642d09537d0128986dfbacc5b1473e02dc6920b
Ahamed Raza <ahamedraza2233@gmail.com>
2024-03-31 11:22:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use('/users', userRoutes);
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+    origin: 'https://bootstap-mern-chat-frontend.vercel.app/'
     methods: ['GET', 'POST']
   }
 });
