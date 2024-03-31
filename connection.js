0e785279a8f236f818259d5dfd0411bdea5eb1a2
Ahamed Raza <ahamedraza2233@gmail.com>
2024-03-31 11:11:04 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL,{
-// mongoose.connect("mongodb+srv://ahmedalfateh:jdFbeEY7boPNU7uK@cluster0.h3j6yyv.mongodb.net/bootstrap-chats",{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
